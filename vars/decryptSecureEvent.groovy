d008fddbea9362458d0574130144abaa7cc99cdc
Matt Elgin <melgin@cloudbees.com>
2020-10-12 14:43:00 -0400

    remove unnecessary variable

--- vars/decryptSecureEvent.groovy
+++ vars/decryptSecureEvent.groovy
@@ -1,8 +1,8 @@
-def call(String privateKeyCredentialId) {
+def call() {
   def sourceJob = currentBuild?.getBuildCauses()[0]?.event?.source?.buildInfo?.job?.toString().replaceAll('/','-')
   def encryptedText = currentBuild?.getBuildCauses()[0]?.event?.encryptedText?.toString()
 
-  withCredentials([file(credentialsId: "${privateKeyCredentialId}", variable: "privateKey")]) {
+  withCredentials([file(credentialsId: "${sourceJob}", variable: "privateKey")]) {
     sh "echo ${encryptedText} | base64 -d | openssl rsautl -inkey ${privateKey} -decrypt"
   }  
 }

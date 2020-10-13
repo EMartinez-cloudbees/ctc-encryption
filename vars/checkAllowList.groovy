521f6ef85a8c49ffd06b790b2811eb0c5b30f821
Matt Elgin <melgin@cloudbees.com>
2020-10-13 17:19:46 -0400

    move allowList to resources

---
+++ vars/checkAllowList.groovy
@@ -0,0 +1,15 @@
+def call() {
+  def allowList = libraryResource 'allowed-jobs.txt'
+
+  def sourceJob = currentBuild?.getBuildCauses()[0]?.event?.source?.buildInfo?.job?.toString()
+
+  allowList.eachLine {
+    if (it == "${sourceJob}") {
+      echo "${sourceJob} in list of allowed jobs."
+      return true
+    } else {
+      echo "${sourceJob} NOT in list of allowed jobs."
+      return false
+    }
+  }
+}

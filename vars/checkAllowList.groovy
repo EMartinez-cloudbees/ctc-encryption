3e5f17addf2e2499bf6c9477b7bfc8234ae24850
Matt Elgin <melgin@cloudbees.com>
2020-10-14 09:36:26 -0400

    split string into list

--- vars/checkAllowList.groovy
+++ vars/checkAllowList.groovy
@@ -1,7 +1,9 @@
 def call() {
-  def allowList = libraryResource 'allowed-jobs.txt'
-
   def sourceJob = currentBuild?.getBuildCauses()[0]?.event?.source?.buildInfo?.job?.toString()
+
+  def allowListString = libraryResource 'allowed-jobs.txt'
+
+  def allowList = allowListString.split('\n')
 
   return allowList.contains(sourceJob)
 }

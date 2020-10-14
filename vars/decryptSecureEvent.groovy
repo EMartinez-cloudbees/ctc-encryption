05fbfedc64be074500cbcd2fc9cf228ba2175bb8
Matt Elgin <melgin@cloudbees.com>
2020-10-14 09:54:16 -0400

    use privateKey as default

--- vars/decryptSecureEvent.groovy
+++ vars/decryptSecureEvent.groovy
@@ -1,8 +1,7 @@
-def call() {
-  def sourceJob = currentBuild?.getBuildCauses()[0]?.event?.source?.buildInfo?.job?.toString().replaceAll('/','-')
+def call(String privateKeyCredentialsId = "privateKey") {
   def encryptedText = currentBuild?.getBuildCauses()[0]?.event?.encryptedText?.toString()
 
-  withCredentials([file(credentialsId: "${sourceJob}", variable: "privateKey")]) {
+  withCredentials([file(credentialsId: privateKeyCredentialsId, variable: "privateKey")]) {
     sh "echo ${encryptedText} | base64 -d | openssl rsautl -inkey ${privateKey} -decrypt"
   }  
 }
--- vars/publishSecureEvent.groovy
+++ vars/publishSecureEvent.groovy
@@ -1,6 +1,6 @@
-def call(String publicKeyCredentialId, String secretString) {
+def call(String publicKeyCredentialId = "publicKey", String secretString) {
 
-  withCredentials([file(credentialsId: "${publicKeyCredentialId}", variable: "publicKey")]) {
+  withCredentials([file(credentialsId: publicKeyCredentialId, variable: "publicKey")]) {
     def encryptedText = sh(script: "echo ${secretString} | openssl rsautl -inkey ${publicKey} -pubin -encrypt | base64 -w 0",
                            returnStdout: true).trim()
     publishEvent event: jsonEvent("{'jobType':'encrypted', 'encryptedText': '${encryptedText}'}"), verbose: true
